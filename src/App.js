6baaafb7aad62f6d7d7451b59a069bb5b3c7ac58
marouane el aatouani <marouane.jimu@gmail.com>
2023-03-17 03:46:15 +0100

    front-end done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
+import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './layout/Navbar';
-import Home from './pages/Home';
-import AddUser from './users/AddUser';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddUser from "./users/AddUser";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/adduser" element={<AddUser />} />
-      </Routes>
+        <Navbar />
+
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/adduser" element={<AddUser />} />
+          <Route exact path="/edituser/:id" element={<EditUser />} />
+          <Route exact path="/viewuser/:id" element={<ViewUser />} />
+        </Routes>
       </Router>
-      
     </div>
   );
 }
